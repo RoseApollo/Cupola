87f56d55deff427b028ba9aa4d4ed55a92e30f40
AUnicornWithNoLife <55228370+AUnicornWithNoLife@users.noreply.github.com>
2023-02-12 23:23:10 +0000

    Tranquility

---
+++ Tranquility/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System.Windows;
+using Microsoft.WindowsAPICodePack.Dialogs;
+using WinRT;
+
+namespace Tranquility
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void LoadFilesClick(object sender, RoutedEventArgs e)
+        {
+            string file = OpenFolder();   
+        }
+
+        private static string? OpenFolder()
+        {
+            CommonOpenFileDialog dialog = new CommonOpenFileDialog();
+            dialog.IsFolderPicker = true;
+            CommonFileDialogResult result = dialog.ShowDialog();
+
+            if (result == CommonFileDialogResult.Ok)
+                return dialog.FileName;
+
+            return null;
+        }
+    }
+}
